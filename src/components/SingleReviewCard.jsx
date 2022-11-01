041dff521dbb958f2e2ade72010cf563bee4c4c7
Joe <joefuller042@gmail.com>
2022-11-01 10:19:16 +0000

    update SingleReviewCard to use alternatives to <div>

--- src/components/SingleReviewCard.jsx
+++ src/components/SingleReviewCard.jsx
@@ -1,7 +1,7 @@
 const SingleReviewCard = ({ review }) => {
   return (
-    <div className="blog-container">
-      <div className="blog-header">
+    <main className="blog-container">
+      <section className="blog-header">
         <div
           style={{
             background: `url(${review.review_img_url}`,
@@ -15,22 +15,22 @@ const SingleReviewCard = ({ review }) => {
             <h3>{review.owner}</h3>
           </div>
         </div>
-      </div>
-      <div className="blog-body">
+      </section>
+      <section className="blog-body">
         <div className="blog-title">
           <h1>{review.title}</h1>
         </div>
-        <div className="single-review-body">
+        <article className="single-review-body">
           <p>{review.review_body}</p>
-        </div>
-        <div className="blog-footer">
+        </article>
+        <footer className="blog-footer">
           <ul>
             <li className="published-date">{review.created_at.slice(0, 10)}</li>
           </ul>
-        </div>
+        </footer>
         <p>votes: {review.votes}</p>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
